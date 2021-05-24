e25087eb61d4af94c702011ce16d27728ba7cf61
Carlos Revilla <clro26.k@gmail.com>
2021-05-24 18:24:47 +0200

    :sparkles: refactor compoent, decorator

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Hero } from "../hero";
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  selector: "app-hero-detail",
+  templateUrl: "./hero-detail.component.html",
+  styleUrls: ["./hero-detail.component.scss"],
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
